f49ac21e405fa07f659d72268f1e15f60f48d318
quanganh24052004 <anh.nguyenquang24052004@gmail.com>
2025-07-31 13:56:58 +0700

    Initial Commit

---
+++ PictureAndVideo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PictureAndVideo
+//
+//  Created by iKame Elite Fresher 2025 on 7/31/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
